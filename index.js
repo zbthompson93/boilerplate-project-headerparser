322986e19387f632b9859e7514db9665215739f8
zbthompson93 <zbthompson93@gmail.com>
2024-07-31 14:50:37 -0600

    finished project

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/api/hello', function (req, res) {
 });
 
 app.get("/api/whoami", (req, res) =>{
-  res.json({"ipadress": req.ip, "language": req.headers["accept-language"], "software": req.headers["user-agent"]});
+  res.json({"ipaddress": req.ip, "language": req.headers["accept-language"], "software": req.headers["user-agent"]});
 })
 
 // listen for requests :)
