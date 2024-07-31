83d154026e8ce0dbcac1b9f2800d3622521fbe37
zbthompson93 <zbthompson93@gmail.com>
2024-07-31 14:48:59 -0600

    finished project

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", (req, res) =>{
+  res.json({"ipadress": req.ip, "language": req.headers["accept-language"], "software": req.headers["user-agent"]});
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
